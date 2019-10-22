aebd587f6de15bde5b8442bcb261de043754adbe
Sam <sam@riversagile.com>
2019-10-22 14:57:42 -0400

    added material and the base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListListComponent } from './list-list/list-list.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { ListFormComponent } from './list-form/list-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListListComponent,
+    MovieListComponent,
+    ListFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-list/list-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-list',
+  templateUrl: './list-list.component.html',
+  styleUrls: ['./list-list.component.scss']
+})
+export class ListListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
